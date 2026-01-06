8c3dba61846701b06157095d03855805e7f6af6e
Rafael <Rafaeldamasceno300407@gmail.com>
2026-01-06 19:39:45 -0300

    Opção de confirmar para devolver o jogo e quantidade de jogod alugados no console

--- js/app.js
+++ js/app.js
@@ -4,12 +4,24 @@ function alterarStatus(id) {
     let botao = gameClicado.querySelector('.dashboard__item__button'); 
 
     if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
+
+        let confirmarDevolucao = confirm('Tem certeza que deseja devolver este jogo?');
+
+        if (confirmarDevolucao) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+            imprimirJogosAlugados();
+        }
+
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
+        imprimirJogosAlugados();
     }
+}
+function imprimirJogosAlugados() {
+    let jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented');
+    console.log(`Quantidade de jogos alugados: ${jogosAlugados.length}`);
 }
